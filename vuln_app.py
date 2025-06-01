fcaeaabe61b3d92d9c009f8c84b7712949a6123e
sjctraining <42831636+sjctraining@users.noreply.github.com>
2025-06-01 21:52:42 +0530

    Update vuln_app.py

--- vuln_app.py
+++ vuln_app.py
@@ -24,3 +24,41 @@ def ping():
     response = os.popen(f"ping -c 1 {host}").read()
     return response
 
+# 🚨 Vulnerability 4: Insecure deserialization
+@app.route('/deserialize', methods=['POST'])
+def deserialize():
+    import pickle
+    data = request.data
+    obj = pickle.loads(data)
+    return str(obj)
+
+# 🚨 Vulnerability 5: Insecure use of eval
+@app.route('/eval', methods=['GET'])
+def evaluate():
+    expr = request.args.get('expr', '')
+    return str(eval(expr))
+
+# 🚨 Vulnerability 6: Poor authentication check
+@app.route('/login', methods=['POST'])
+def login():
+    user = request.form.get('username')
+    pwd = request.form.get('password')
+    if user == USERNAME and pwd == PASSWORD:
+        return "Login successful!"
+    else:
+        return "Invalid credentials"
+
+# 🚨 Vulnerability 7: Sensitive data exposure
+@app.route('/debug', methods=['GET'])
+def debug():
+    return f"Debug info: USERNAME={USERNAME}, PASSWORD={PASSWORD}"
+
+# 🚨 Vulnerability 8: Unvalidated redirect
+@app.route('/redirect', methods=['GET'])
+def redirect_user():
+    from flask import redirect
+    target = request.args.get('url')
+    return redirect(target)
+
+if __name__ == '__main__':
+    app.run(debug=True)
